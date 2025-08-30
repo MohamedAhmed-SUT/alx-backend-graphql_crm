6bc545a576b0db96a23ea49ff918d3e26aead949
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-08-30 17:03:38 +0300

    update

--- crm/settings.py
+++ crm/settings.py
@@ -35,8 +35,21 @@ schema = graphene.Schema(mutation=Mutation)
 INSTALLED_APPS = [
     # ...
     "django_crontab",
+    "django_celery_beat",
 ]
+
 
 CRONJOBS = [
     ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
+
+
+from celery import crontab
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
