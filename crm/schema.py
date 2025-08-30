02929f15d7c482d8e0a68b1f818107c521757f2e
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-08-30 16:58:32 +0300

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -46,3 +46,38 @@ class Query(graphene.ObjectType):
         if order_by:
             qs = qs.order_by(*order_by)
         return qs
+import graphene
+from graphene_django import DjangoObjectType
+from crm.models import Product   # <= لازم تكون كده
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = ("id", "name", "stock")
+
+class UpdateLowStockProducts(graphene.Mutation):
+    # <= لازم يكون الاسم كده بالضبط
+    class Arguments:
+        pass
+
+    success = graphene.String()
+    products = graphene.List(ProductType)
+
+    @classmethod
+    def mutate(cls, root, info):
+        low_stock = Product.objects.filter(stock__lt=10)
+        updated_products = []
+        for product in low_stock:
+            product.stock += 10   # <= هنا يظهر الرقم 10
+            product.save()
+            updated_products.append(product)
+
+        return UpdateLowStockProducts(
+            success="Low stock products updated successfully!",
+            products=updated_products,
+        )
+
+class Mutation(graphene.ObjectType):
+    update_low_stock_products = UpdateLowStockProducts.Field()
+
+schema = graphene.Schema(mutation=Mutation)
